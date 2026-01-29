eea82417e9f563a927dc40efc43c6c95879fb742
hoanghuu310-lab <hoanghuu310@gmail.com>
2026-01-29 16:59:05 +0700

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,68 +1,65 @@
-const express = require('express');
-let books = require("./booksdb.js");
-let isValid = require("./auth_users.js").isValid;
-let users = require("./auth_users.js").users;
-const public_users = express.Router();
+const axios = require('axios');
 
-public_users.post("/register", (req,res) => {
-  const { username, password } = req.body;
-  if (username && password) {
-    if (!users.find(u => u.username === username)) {
-      users.push({"username":username,"password":password});
-      return res.status(200).json({message: "User successfully registred. Now you can login"});
+/* Task 10: Get all books – Using async/await */
+async function getAllBooks() {
+    try {
+        const response = await axios.get('http://localhost:5000/');
+        console.log(JSON.stringify(response.data, null, 4));
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching books:", error.message);
+        return null;
     }
-    return res.status(404).json({message: "User already exists!"});
-  }
-  return res.status(404).json({message: "Unable to register user."});
-});
+}
 
-// Task 10: Get book list using Promises
-public_users.get('/', function (req, res) {
-  const getBooks = new Promise((resolve) => {
-    resolve(res.status(200).json(books));
-  });
-  return getBooks;
-});
+/* Task 11: Get book details based on ISBN – Using Promise */
+function getBookByISBN(isbn) {
+    axios.get(`http://localhost:5000/isbn/${isbn}`)
+    .then(function(response) {
+        console.log(JSON.stringify(response.data, null, 4));
+        return response.data;
+    })
+    .catch(function(error) {
+        console.error("Error fetching book by ISBN:", error.message);
+        return null;
+    });
+}
 
-// Task 11: Get book details by ISBN using Promises
-public_users.get('/isbn/:isbn', function (req, res) {
-  const isbn = req.params.isbn;
-  const getByIsbn = new Promise((resolve, reject) => {
-    if (books[isbn]) {
-      resolve(res.status(200).json(books[isbn]));
-    } else {
-      reject(res.status(404).json({message: "Book not found"}));
-    }
-  });
-  return getByIsbn;
-});
+/* Task 12: Get book details based on Author – Using Promise */
+function getBookByAuthor(author) {
+    axios.get(`http://localhost:5000/author/${author}`)
+    .then(function(response) {
+        console.log(JSON.stringify(response.data, null, 4));
+        return response.data;
+    })
+    .catch(function(error) {
+        console.error("Error fetching books by author:", error.message);
+        return null;
+    });
+}
 
-// Task 12: Get book details by author using Promises
-public_users.get('/author/:author', function (req, res) {
-  const author = req.params.author;
-  const getByAuthor = new Promise((resolve) => {
-    let results = Object.values(books).filter(b => b.author === author);
-    resolve(res.status(200).json({booksbyauthor: results}));
-  });
-  return getByAuthor;
-});
+/* Task 13: Get book details based on Title – Using Promise */
+function getBookByTitle(title) {
+    axios.get(`http://localhost:5000/title/${title}`)
+    .then(function(response) {
+        console.log(JSON.stringify(response.data, null, 4));
+        return response.data;
+    })
+    .catch(function(error) {
+        console.error("Error fetching books by title:", error.message);
+        return null;
+    });
+}
 
-// Task 13: Get book details by title using Promises
-public_users.get('/title/:title', function (req, res) {
-  const title = req.params.title;
-  const getByTitle = new Promise((resolve) => {
-    let results = Object.values(books).filter(b => b.title === title);
-    resolve(res.status(200).json({booksbytitle: results}));
-  });
-  return getByTitle;
-});
+module.exports = {
+    getAllBooks,
+    getBookByISBN,
+    getBookByAuthor,
+    getBookByTitle
+};
 
-public_users.get('/review/:isbn', function (req, res) {
-  const isbn = req.params.isbn;
-  if (books[isbn]) {
-    return res.status(200).json(books[isbn].reviews);
-  }
-  return res.status(404).json({message: "Not found"});
-});
-
-module.exports.general = public_users;
+/* Testing the functions */
+getAllBooks();
+getBookByISBN(1);
+getBookByAuthor("Chinua Achebe");
+getBookByTitle("Things Fall Apart");
